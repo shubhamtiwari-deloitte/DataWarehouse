e22903831d06c07030c04469ad7f3c4bd63f0944
shubhamtiwari-deloitte <shubhamtiwari7@deloitte.com>
2022-10-27 04:14:14 -0700

    Product Controller and Services

---
+++ Producer/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Producer.Models;
+using Producer.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Producer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        ProductService productService= new ProductService();
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return productService.getAllProducts();
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public Product Get(string id)
+        {
+            return productService.getProductById(id);
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public Product Post([FromBody] string productName, [FromBody] int qty)
+        {
+            return productService.saveProduct(productName, qty);
+        }
+
+    }
+}
---
+++ Producer/Services/ProductService.cs
@@ -0,0 +1,71 @@
+﻿using Producer.Database;
+using Producer.Models;
+
+namespace Producer.Services
+{
+    public class ProductServices
+    {
+        DatabaseContext db;
+
+        public ProductServices()
+        {
+            db = new DatabaseContext();
+        }
+
+        public IEnumerable<Product> getAllProducts()
+        {
+            return db.products.ToList();
+        }
+
+        public Product getProductById(string id)
+        {
+            try
+            {
+                return db.products.SingleOrDefault(p => p.productId == id);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public Product saveProduct(string productName, int quantity)
+        {
+            try
+            {
+                Product product = new Product()
+                {
+                    productId = Guid.NewGuid().ToString(),
+                    productName = productName,
+                    quantity = quantity
+                };
+
+                db.products.Add(product);
+                return product;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+                return null;
+            }
+        }
+
+
+        public Product saveProductToInventory(string id)
+        {
+            Product product=this.getProductById(id);
+            if (product!=null)
+            {
+                /*
+                 * Code for saving product to inventory
+                 */
+                return product;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
